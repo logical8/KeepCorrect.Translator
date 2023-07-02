5e30e9413cbbc4e2850d42eaa2a7a5f1e9b5c4ad
alekkireev <alekkireev@gmail.com>
2023-07-03 00:58:25 +0400

    Add Form1 with tray icon

--- KeepCorrect.Translator/Form1.cs
+++ KeepCorrect.Translator/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using KeepCorrect.Translator.Properties;
 
 namespace KeepCorrect.Translator
 {
@@ -15,6 +9,36 @@ namespace KeepCorrect.Translator
         public Form1()
         {
             InitializeComponent();
+            WindowState = FormWindowState.Minimized;
+            ShowInTaskbar = false;
+            
+            // Initialize Tray Icon
+            _trayIcon = new NotifyIcon
+            {
+                Icon = Resources.Icon1,
+                ContextMenu = new ContextMenu(new [] 
+                {
+                    new MenuItem("Show", Show),
+                    new MenuItem("Exit", Exit)
+                }),
+                Visible = true
+            };
+        }
+        
+        private readonly NotifyIcon _trayIcon;
+
+        void Show(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+                WindowState = FormWindowState.Normal;
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            _trayIcon.Visible = false;
+
+            Application.Exit();
         }
     }
 }
