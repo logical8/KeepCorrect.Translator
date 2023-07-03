15fbaf8b5b7a808fd522e6444427eb6dd60d8ba1
alekkireev <alekkireev@gmail.com>
2023-07-03 21:21:41 +0400

    Combination Alt + Shift + T makes Form1 visible

--- KeepCorrect.Translator/Form1.cs
+++ KeepCorrect.Translator/Form1.cs
@@ -11,34 +11,42 @@ namespace KeepCorrect.Translator
             InitializeComponent();
             WindowState = FormWindowState.Minimized;
             ShowInTaskbar = false;
-            
+
             // Initialize Tray Icon
             _trayIcon = new NotifyIcon
             {
                 Icon = Resources.Icon1,
-                ContextMenu = new ContextMenu(new [] 
+                ContextMenu = new ContextMenu(new[]
                 {
                     new MenuItem("Show", Show),
                     new MenuItem("Exit", Exit)
                 }),
                 Visible = true
             };
+
+            GlobalHotKey.RegisterHotKey(Modifiers.Alt | Modifiers.Shift, Keys.T, ShowFormAndTranslate);
         }
-        
+
         private readonly NotifyIcon _trayIcon;
 
-        void Show(object sender, EventArgs e)
+        private void Show(object sender, EventArgs e)
         {
             if (WindowState == FormWindowState.Minimized)
                 WindowState = FormWindowState.Normal;
         }
 
-        void Exit(object sender, EventArgs e)
+        private void Exit(object sender, EventArgs e)
         {
             // Hide tray icon, otherwise it will remain shown until user mouses over it
             _trayIcon.Visible = false;
 
             Application.Exit();
+        }
+
+        private void ShowFormAndTranslate()
+        {
+            if (WindowState == FormWindowState.Minimized)
+                WindowState = FormWindowState.Normal;
         }
     }
 }
